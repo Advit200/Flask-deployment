33f320647ca1473764ecb30942914a62aae0fe16
Aditya Gaurav <50029051+Advit200@users.noreply.github.com>
2020-10-28 20:56:03 +0530

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,19 +1,23 @@
 from flask import Flask , jsonify
 from flask_restful import Api,Resource,reqparse
+import pandas as pd
 
 app=Flask(__name__)
 api=Api(app)
 
-data={1:{'first name':'Aditya','second name':'Gaurav','college':"VIT Vellore",'gf':False},
-2:{'first name':'shradha','second name':'suman','college':"BIT Mesra",'bf':False}}
+# data={1:{'first name':'Aditya','second name':'Gaurav','college':"VIT Vellore",'gf':False},
+# 2:{'first name':'shradha','second name':'suman','college':"BIT Mesra",'bf':False}}
+path=r'C:\Users\a.a.gaurav\Desktop\DAM\ADM-templatization\testing 2\Output_Files\Detailed_report\AP322_Executive Sales and Operations  Planning_1909__DetailedReport.xlsx'
+df=pd.read_excel(path,sheet_name='Doc Summary')
+x=df.to_dict('index')
+
 
 class Trying(Resource):
 
     def get(self,id):
-        result=data.get(id)
+        global x
+        result=x.get(id)
         return jsonify(result)
-
-
 
 api.add_resource(Trying,"/home/<int:id>")
 
