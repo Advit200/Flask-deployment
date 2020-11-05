de7cee35363240dc1c96459c06f709df4dfdbe04
Aditya Gaurav <50029051+Advit200@users.noreply.github.com>
2020-11-05 17:39:07 +0530

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,14 +1,24 @@
 from flask import Flask , jsonify
 from flask_restful import Api,Resource,reqparse
 import pandas as pd
+import os
 
 app=Flask(__name__)
 api=Api(app)
 
 # data={1:{'first name':'Aditya','second name':'Gaurav','college':"VIT Vellore",'gf':False},
 # 2:{'first name':'shradha','second name':'suman','college':"BIT Mesra",'bf':False}}
-path=r'AP322_Executive Sales and Operations  Planning_1909__DetailedReport.xlsx'
-df=pd.read_excel(path,sheet_name='Doc Summary')
+path_name = r"\\ts.accenture.com@SSL\DavWWWRoot\sites\softwares419\Shared Documents\General"
+
+files_list = []
+
+for files in os.listdir(path_name):
+	if os.path.splitext(files) == ".xlsx":
+		files_list.append(files)
+
+
+path=files_list[0]
+df=pd.read_excel(path,sheet_name='Asset')
 x=df.to_dict('index')
 
 
